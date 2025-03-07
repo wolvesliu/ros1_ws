a8891fca903de5fa0c5c0f564805ba3c385697ef
Max Liu <45615769+wolvesliu@users.noreply.github.com>
2025-03-07 17:34:48 +0800

    Update listener.cpp

--- ros_topic_cpp_ws/src/topic_cpp/src/listener.cpp
+++ ros_topic_cpp_ws/src/topic_cpp/src/listener.cpp
@@ -3,9 +3,9 @@
 
 //回调函数，当有新消息到达chatter话题时它就会被调用
 //回调函数不能做太多事情，否则会一直运行，导致下一个触发始终不能再来运行，所以回调函数要高效
-void chatterCallback(const std_msgs::String::ConstPtr& msg)
+void chatterCallback(const std_msgs::String::ConstPtr& msg) //std_msgs::String是消息的结构，ConstPtr常指针，msg是以常指针的形式指向std_msgs::String
 {
-  ROS_INFO("I heard: [%s]", msg->data.c_str());
+  ROS_INFO("I heard: [%s]", msg->data.c_str());//打印 指针类型调用
 }
 
 int main(int argc, char **argv)
@@ -17,6 +17,7 @@ int main(int argc, char **argv)
   //通过主节点订阅chatter话题.每当有新消息到达时，ROS将调用chatterCallback()函数.第二个参数是队列大小
   ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);//创建订阅者
 
+  //等待消息传来，消息没来就等待，消息来了就去执行回调函数。
   ros::spin();//启动了一个自循环，一直循环等待回调函数，不会到下面打return 0
 
   return 0;
